c546b312d9a66fd419de82484d2799c9c801b9ca
linw <770601773@qq.com>
2018-06-23 00:10:53 +0800

    设备方向监听

--- UI Train/UI Train/ViewController.swift
+++ UI Train/UI Train/ViewController.swift
@@ -19,7 +19,30 @@ class ViewController: UIViewController {
         let imgView = UIImageView(image: img)
         self.view.addSubview(imgView)
         
+        // 捕获屏幕方向切换事件
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.orientationChange(_:)), name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
+    }
     
+    @objc func orientationChange(_ notification: Notification) {
+        // 获得当前运行的设备
+        let device = UIDevice.current
+        switch device.orientation
+        {
+        case .portrait:
+            print("面向设备保存垂直，home 键位于下方")
+        case .portraitUpsideDown:
+            print("面向设备保存垂直，home 键位于上方")
+        case .landscapeLeft:
+            print("面向设备保存垂直，home 键位于左侧")
+        case .landscapeRight:
+            print("面向设备保存垂直，home 键位于右侧")
+        case .faceUp:
+            print("设备平放，home 键朝上")
+        case .faceDown:
+            print("设备平放，home 键朝下")
+        case .unknown:
+            print("方向未知")
+        }
     }
 
     override func didReceiveMemoryWarning() {
